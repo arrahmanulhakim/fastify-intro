531f8ff938b0da9eda53b23ac100440ac08fa109
Arrahmanul Hakim <hakimarrahmannul@gmail.com>
2023-10-31 11:32:05 +0700

    f

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const fastify = require('fastify')
+
+const Ajv = require('ajv')
+
+const ajv = new Ajv({
+    //   removeAdditional: 'all',
+    coerceTypes: 'array',
+})
+fastify.setValidatorCompiler(({ schema, method, url, httpPart }) => {
+    return ajv.compile(schema)
+})
+
+const app = fastify({ logger: true })
+
+app.register(require('@fastify/formbody')) // <== fastify-formbody is deprecated
+
+app.register(require('./routers'), {
+    prefix: '/api',
+})
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT).catch((error) => {
+    app.log.error(error)
+    process.exit()
+})
